2abd895fc60fbc932aa1f0ba491bc6e1915392c2
SzivakMilan <milanszivak@gmail.com>
2023-09-26 21:53:39 +0200

    this works

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 function logger(param) {
     console.log(param)
-}
-
-// FÜGGVÉNY MEGHÍVÁSA, CSAK ILYENKOR FUT LE
-
-let result = logger(100)
-console.log(result)
-
-function sumOfTwoNumbers(number1, number2) {
-    //console.log(number1 + number2) 
+  }
+  
+  logger("kicsmacska") // FÜGGVÉNY MEGHÍVÁSA -> ILYENKOR (ÉS CSAK ILYENKOR) FUT LE
+  
+  function sumOfTwoNumbers(number1, number2) {
+    //console.log(number1 + number2)
     let sum = number1 + number2
-    return sum
-}
-
-logger(sumOfTwoNumbers)
+    return sum // ITT TÉR VISSZA A FÜGGVÉNY AZ EREDMÉNNYEL
+  }
+  
+  /* let result = sumOfTwoNumbers(2, 4)
+  logger(result) */
+  
+  logger(sumOfTwoNumbers(5, 10))

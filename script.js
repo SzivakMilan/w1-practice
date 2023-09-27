5005227da27cf3807aa071541bc6c8382441d576
SzivakMilan <milanszivak@gmail.com>
2023-09-27 12:48:05 +0200

    wednesday hiányos

--- script.js
+++ script.js
@@ -1,16 +1,37 @@
-function logger(param) {
-    console.log(param)
-  }
-  
-  logger("kicsmacska") // FÜGGVÉNY MEGHÍVÁSA -> ILYENKOR (ÉS CSAK ILYENKOR) FUT LE
-  
-  function sumOfTwoNumbers(number1, number2) {
-    //console.log(number1 + number2)
-    let sum = number1 + number2
-    return sum // ITT TÉR VISSZA A FÜGGVÉNY AZ EREDMÉNNYEL
-  }
-  
-  /* let result = sumOfTwoNumbers(2, 4)
-  logger(result) */
-  
-  logger(sumOfTwoNumbers(5, 10))
+let variable = "kismacska"
+let variable2 = 101
+let variable3 = true
+let variable4 = null
+//        index        0         1         2         3   Az index mindig nullától indul
+let variableArray = ["alma", "barack", "szilva", "vegyes"]
+let variableArray2 =[1, 2, 3, 4]
+let variableArray3 = ["auto", 54, true, undefined, false, "62"]
+let variableArray4 = [variable, variable2, variable3, variable4]
+
+console.log(variableArray[0])
+console.log(variableArray[1])
+console.log(variableArray[2])
+console.log(variableArray[3])
+
+let counter = 0
+
+while (counter <= 5){
+  console.log(counter)
+  counter++ // = counter = counter +1 */
+ }
+
+let X = 1
+
+if (x === 2){
+
+}
+
+for (let counter = 0; counter <= 5; counter++) {
+  console.log(counter)
+}
+
+for (let index = 0; index < array.lenght; index++){
+  console.log(array[index])
+}
+
+console.log(varableArray3.lenght)
